89f7846a27a0bed38912a9f086622de696d22c9d
arnevgarg <arnevgarg@gmail.com>
2024-06-20 12:44:00 +0530

    Update Cuboid3D.js
    lighting improve

--- frontend/src/components/Cuboid3D.js
+++ frontend/src/components/Cuboid3D.js
@@ -8,9 +8,16 @@ const Cuboid3D = ({ capturedImage }) => {
 
     const createScene = () => {
       const scene = new BABYLON.Scene(engine);
-      const camera = new BABYLON.ArcRotateCamera("camera1", 0, 0, 10, BABYLON.Vector3.Zero(), scene);
+      const camera = new BABYLON.ArcRotateCamera("Camera", 3 * Math.PI / 2, Math.PI / 8, 3, BABYLON.Vector3.Zero(), scene);
+
       camera.attachControl(canvas, true);
-      const light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
+    
+      const light = new BABYLON.HemisphericLight("hemi", new BABYLON.Vector3(0, 1, 0), scene);
+      var light2 = new BABYLON.PointLight(
+        "light2",
+        new BABYLON.Vector3(0, 10, 0),
+        scene
+      );
       
       const box = BABYLON.MeshBuilder.CreateBox("box", {}, scene);
 
